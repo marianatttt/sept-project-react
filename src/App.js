4b428433a6ad85f4afca47454c243e0062cb4c3f
marianatttt <malunka221988@gmail.com>
2023-02-02 19:01:41 +0200

    lesson5, part1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {ClassComponentComments, ClassComponentPosts} from "./components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+ return (
+  <div>
+      {/*<ClassComponentPosts/>*/}
 
-export default App;
+   <ClassComponentComments/>
+
+  </div>
+ );
+};
+
+export {App};
